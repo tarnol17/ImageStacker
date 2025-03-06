d52155aebc2479c2aa5c86b6ffaf41e89edd2471
Tyson Arnold <tarnol17@ut.utm.edu>
2025-03-06 22:32:33 +0000

    added to stacker.h

--- stacker.h
+++ stacker.h
@@ -1,1 +1,24 @@
+#ifndef STACKER_H
+#define STACKER_H
 
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+
+class Stacker {
+private:
+    std::string magic_number;
+    int width, height, max_color;
+    std::vector<Pixel> pixels;
+    struct Pixel {
+        int red, green, blue;
+    };
+
+public:
+    Stacker();
+    bool loadImage(const std::string& filename);
+    void writeOutput(const std::string& outputFilename) const;
+};
+
+#endif

f915bf0f3744a7011b907dd89465389d4f33a8da
cmcken14 <cmcken14@ut.utm.edu>
2025-03-08 20:34:38 -0600

    Added stacker.h implementation

--- stacker.h
+++ stacker.h
@@ -1,3 +1,12 @@
+/**
+ * @file stacker.h
+ * @author Christian McKenzie / Tyson Arnold
+ * @date 2025-03-07
+ * @brief Stacker Class
+ * 
+ * Reads, averages, adn writes PPM images
+ */
+
 #ifndef STACKER_H
 #define STACKER_H
 
@@ -6,19 +15,32 @@
 #include <vector>
 #include <string>
 
+struct Pixel{
+  int red, green, blue;
+}
+  
 class Stacker {
 private:
-    std::string magic_number;
-    int width, height, max_color;
-    std::vector<Pixel> pixels;
-    struct Pixel {
-        int red, green, blue;
-    };
+  std::string magic_number;
+  int width, height, max_color;
+  std::vector<Pixel> pixels;
 
 public:
-    Stacker();
-    bool loadImage(const std::string& filename);
-    void writeOutput(const std::string& outputFilename) const;
+  // Constructor
+  Stacker();
+
+  /* Reads the PPM image file and accumulates its pixel values
+   * @first Shows if its the first image
+  */
+  bool loadImage(const std::string& filename, bool first);
+
+  /* Averages the pixel values
+   * @numImages the total number of images processed
+  */
+  void averagePixels(int numImages);
+
+  // Saves the final averages image to a PPM file
+  bool saveImage(const std::string& filename);
 };
 
 #endif
